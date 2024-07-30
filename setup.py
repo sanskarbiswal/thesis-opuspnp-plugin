09a676a884c09cc8909c0b8a3fdbe4e8aea8378a
Sanskar Biswal <sanskarbiswal@gmail.com>
2024-07-30 13:38:02 +0200

    Added Open CV requirement to Setup

--- setup.py
+++ setup.py
@@ -33,7 +33,9 @@ plugin_url = "https://github.com/sanskarbiswal/thesis-opuspnp-plugin"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = [
+    "opencv-python",
+]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
